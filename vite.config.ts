5a52859aff17fbd93f4408ecfdf37dcd39afe2ef
Adityanarayan Tiwari <adityant17121997@gmail.com>
2023-09-24 23:23:27 +0530

    base removed

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smt-client/',
-  // server: {
-  //   port: 3000
-  // }
+  // base: '/smt-client/',
+  server: {
+    port: 3000
+  }
 })
